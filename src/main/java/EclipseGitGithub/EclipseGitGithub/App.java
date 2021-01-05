2a6abdc8353e797ed2794d540c3c37dcbdfe1a93
Sarnault <sa.arnault@gmail.com>
2021-01-05 14:37:49 +0100

    Commit 1

---
+++ src/main/java/EclipseGitGithub/EclipseGitGithub/App.java
@@ -0,0 +1,13 @@
+package EclipseGitGithub.EclipseGitGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour EPSI 1!" );
+    }
+}
